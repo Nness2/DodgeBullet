bdff8dc407ddd28a42275d54459c27382d01db78
Aness Menaa <menaa.aness2@gmail.com>
2020-11-02 01:03:42 +0100

    Zone ok

--- Assets/Script/ZoneLimitations.cs
+++ Assets/Script/ZoneLimitations.cs
@@ -11,14 +11,23 @@ public class ZoneLimitations : MonoBehaviour
     private GameObject NeutreField1;
     private GameObject NeutreField2;*/
     public CharacterController controller;
-    public List<GameObject> SpawnsZone;
+    public List<GameObject> BlueSpawnsZone;
+    public List<GameObject> RedSpawnsZone;
+
 
     public bool teamBlue;
     public int state = 0;
 
+    private Health health;
+
+    private IEnumerator coroutine;
     // Start is called before the first frame update
     void Start()
     {
+
+        health = GetComponent<Health>();
+
+
         /*BlueField = GameObject.FindGameObjectWithTag("BlueField");
         RedField = GameObject.FindGameObjectWithTag("RedField");
         BluePrison = GameObject.FindGameObjectWithTag("BluePrison");
@@ -33,47 +42,122 @@ public class ZoneLimitations : MonoBehaviour
         
     }
 
-    /*void OnTriggerStay(Collider collision)
+    void OnTriggerEnter(Collider collision)
     {
+        StopAllCoroutines();
+
         if (collision.gameObject.tag == "BlueField" && !teamBlue || collision.gameObject.tag == "BlueField" && teamBlue && state != 0)
         {
-            Debug.Log("35 Damages");
+            coroutine = ZoneDamage(35);
+            StartCoroutine(coroutine);
+            //health.TakeDamage(35);
+            //Debug.Log("35 Damages");
         }
 
         if (collision.gameObject.tag == "RedField" && teamBlue || collision.gameObject.tag == "RedField" && !teamBlue && state != 0)
         {
-            Debug.Log("35 Damages");
+            coroutine = ZoneDamage(35);
+            StartCoroutine(coroutine);
+            //health.TakeDamage(35);
+            //Debug.Log("35 Damages");
         }
 
         if (collision.gameObject.tag == "BluePrison" && !teamBlue || collision.gameObject.tag == "BluePrison" && teamBlue && state != 1)
         {
-            Debug.Log("50 Damages");
+            coroutine = ZoneDamage(50);
+            StartCoroutine(coroutine);
+            //health.TakeDamage(50);
+            //Debug.Log("50 Damages");
         }
 
         if (collision.gameObject.tag == "RedPrison" && teamBlue || collision.gameObject.tag == "RedPrison" && !teamBlue && state != 1)
         {
-            Debug.Log("50 Damages");
+            coroutine = ZoneDamage(50);
+            StartCoroutine(coroutine);
+            //health.TakeDamage(50);
+            //Debug.Log("50 Damages");
         }
 
-        if (collision.gameObject.tag == "Neutre1" && state != 2)
+        if (collision.gameObject.tag == "Neutre1" && state != 2 && teamBlue)
         {
-            Debug.Log("20 Damage");
+            coroutine = ZoneDamage(20);
+            StartCoroutine(coroutine);
+            //health.TakeDamage(20);
+            //Debug.Log("20 Damage");
         }
 
-        if (collision.gameObject.tag == "Neutre2" && state != 3)
+        if (collision.gameObject.tag == "Neutre2" && state != 3 && teamBlue)
         {
-            Debug.Log("20 Damage");
+            coroutine = ZoneDamage(20);
+            StartCoroutine(coroutine);
+            //health.TakeDamage(20);
+            //Debug.Log("20 Damage");
         }
 
+
+
+        if (collision.gameObject.tag == "Neutre1" && state != 3 && !teamBlue)
+        {
+            coroutine = ZoneDamage(20);
+            StartCoroutine(coroutine);
+            //health.TakeDamage(20);
+            //Debug.Log("20 Damage");
+        }
+
+        if (collision.gameObject.tag == "Neutre2" && state != 2 && !teamBlue)
+        {
+            coroutine = ZoneDamage(20);
+            StartCoroutine(coroutine);
+            //health.TakeDamage(20);
+            //Debug.Log("20 Damage");
+        }
+        /*if ((collision.gameObject.tag == "BlueField" && state == 0 || collision.gameObject.tag == "BluePrison" && state == 1 || collision.gameObject.tag == "Neutre1" && state == 2 || collision.gameObject.tag == "Neutre2" && state == 3) && teamBlue)
+        {
+            StopAllCoroutines();
+        }
+
+        if ((collision.gameObject.tag == "BlueField" && state == 0 || collision.gameObject.tag == "BluePrison" && state == 1 || collision.gameObject.tag == "Neutre1" && state == 2 || collision.gameObject.tag == "Neutre2" && state == 3) && !teamBlue)
+        {
+            StopAllCoroutines();
+        }*/
+
+    }
+
+    /*void OnTriggerExit(Collider collision)
+    {
+        StopAllCoroutines();
     }*/
 
-    public void NextZone()
+
+        public void NextZone()
     {
-        Debug.Log(state);
-        controller.enabled = false;
-        transform.position = SpawnsZone[state].transform.position;
-        transform.rotation = SpawnsZone[state].transform.localRotation;
-        controller.enabled = true;
+        if (teamBlue)
+        {
+            controller.enabled = false;
+            transform.position = BlueSpawnsZone[state].transform.position;
+            transform.rotation = BlueSpawnsZone[state].transform.localRotation;
+            controller.enabled = true;
+        }
+
+        if (!teamBlue)
+        {
+            controller.enabled = false;
+            transform.position = RedSpawnsZone[state].transform.position;
+            transform.rotation = RedSpawnsZone[state].transform.localRotation;
+            controller.enabled = true;
+        }
+
+    }
+
+    IEnumerator ZoneDamage(int damages)
+    {
+        while (true)
+        {
+            var wait = new WaitForSeconds(1f);
+            health.TakeDamage(damages);
+            yield return wait;
+        }
+
     }
 
 }
